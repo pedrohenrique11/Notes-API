79ebfceb0ee51091103fa012f3e82ccff8f88c84
Pedro Henrique <pedroballack1167@gmail.com>
2024-04-02 09:23:08 -0300

    feat: add database

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import http from 'node:http';
+import { Database } from './database/database.js';
 
+const database = new Database
 
 const server = http.createServer((res, req) => {
+    const { method, url } = res
+
 
 })
 
-server.listen(3333, () => {console.log('servidor funcionando')}) 
+server.listen(1167, () => {console.log('servidor funcionando')}) 
